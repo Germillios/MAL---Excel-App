093ce8108caebb811fed4ff0de52ccf298ca9ff0
Kamil Żmuda <kamilos1946@gmail.com>
2018-11-24 19:01:35 +0100

    Connect to Database, Add ADO.NET Entity Data Model, CollectionVIewSource in Design, in Code, Add DataContext and Window_Loaded -> DataSource appear

--- MAL3/MainWindow.xaml.cs
+++ MAL3/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,21 @@ namespace MAL3
     /// </summary>
     public partial class MainWindow : Window
     {
+        MalAppDBEntities context = new MalAppDBEntities();
+        CollectionViewSource anime_ilosc_ver2ViewSource;
+        CollectionViewSource animelist_1538511199_3585579ViewSource;
         public MainWindow()
         {
             InitializeComponent();
+            anime_ilosc_ver2ViewSource = (CollectionViewSource)FindResource("anime_ilosc_ver2ViewSource");
+            animelist_1538511199_3585579ViewSource = (CollectionViewSource)FindResource("animelist_1538511199_3585579ViewSource");
+            DataContext = this;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            context.anime_ilosc_ver2.Load();
+            anime_ilosc_ver2ViewSource.Source = context.anime_ilosc_ver2.Local;
         }
     }
 }
